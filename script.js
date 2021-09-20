01557d330ce63e1fd69c9ea87b8771ad24f90525
rafaalmeida1 <rafaempresarial312@gmail.com>
2021-09-20 16:32:47 -0300

    Aula Feita

--- script.js
+++ script.js
@@ -14,7 +14,21 @@
 
 // localStorage.removeItem("nome"); //isso removel o dado
 
-function atualizar(element){
+
+//input de dados com string
+
+onload = function () { //quando carregar a pag vai realizar essa function
+
+    //Persistência de Dados
+
+    let nome = localStorage.getItem("nome"); //pego o dado "nome"
+    let h1 = document.getElementById("nome"); //pego novamente o h1
+    h1.innerHTML = nome; // e adiciono o dado que foi recolhido, na tag h1. Agora quando atualizar a pagina vai aparecer o nome que digitei
+
+
+}
+
+function atualizar(element) {
 
     let valor = element.value; //aqui está pegando o input lá do HTML, e atribuindo isso a variavel valor
     console.log(valor);
@@ -22,9 +36,20 @@ function atualizar(element){
     let h1 = document.getElementById("nome");
     h1.innerHTML = valor; //aqui faço tudo que for escrito no input, ser transcrito no h1
 
+    localStorage.setItem("nome", valor);
+
 }
 
 
-//parei em 5:50
 
-//até agora aprendi apenas a transcrever algo do input na tag h1.
+//input com outro tipo de dados
+
+// let a = {nome: "Rafael", n1: 9.4}
+
+// localStorage.setItem("aluno", JSON.stringify(a));
+
+
+let a = localStorage.getItem("aluno");
+console.log(JSON.parse(a)); //transformei aquele objeto aluno, q foi passado como string, em objeto de fato
+
+// localStorage.clear(); // isso apagou tudo que tinha na memória
